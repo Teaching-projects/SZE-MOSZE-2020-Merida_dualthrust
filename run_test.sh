619570145c9622b659c7f51428047f29804165af
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-09-24 19:14:56 +0200

    Remove echos from bash file

--- run_test.sh
+++ run_test.sh
@@ -9,7 +9,4 @@ IFS=$'\n'
 ./a.out units/Tomcat.json units/Tolvaj.json >> results.txt
 ./a.out units/Tomcat.json units/Robin.json >> results.txt
 
-cat results.txt
-cat results_correct.txt
-
 diff results.txt results_correct.txt
