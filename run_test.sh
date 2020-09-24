c62c74b45f3ac3f6ea5dc8636f832ac5211b9d41
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-09-24 19:11:43 +0200

    Print file contents for debugging

--- run_test.sh
+++ run_test.sh
@@ -9,4 +9,7 @@ IFS=$'\n'
 ./a.out units/Tomcat.json units/Tolvaj.json >> results.txt
 ./a.out units/Tomcat.json units/Robin.json >> results.txt
 
+cat results.txt
+cat results_correct.txt
+
 diff results.txt results_correct.txt
