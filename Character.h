1183b5de8e47d92665c42bf5541dc2dfd5deb1c1
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-10-14 14:37:58 +0200

    Updated comments for Doxygen

--- Character.h
+++ Character.h
@@ -1,26 +1,77 @@
+/**
+ * @file Character.h
+ * @author lapatkrisz
+ * @brief Character class
+ * 
+ * This class contains basic methods and data members of the Character class. Character objects in the game can have different HP/ATK values and fight each other.
+ * 
+ * @version 0.3
+ * @date 2020-10-13
+ */
+
 #ifndef CHARACTER_HEADER
 #define CHARACTER_HEADER
 
 #include <string>
 
-/* Character class
-   This class contains basic methods and data members of the Character class.
-   Character objects in the game can have different HP/ATK values and fight each other.
-*/
-
 class Character {
 
 public:
+    /**
+     * @brief Construct a new Character object
+     * In case of no inputs the Character object gets default values
+     */
     Character(const std::string, const int, const int, const double);
+    /**
+     * @brief JSON parse method for creating a Character object based on a given JSON input file
+     * @return Character type unit
+     */
     static Character* parseUnit(const std::string&);
-    std::string const & getName() const;
-    int const & getATK() const;
-    int const & getHP() const;
-    double const & getACD() const;
+    /**
+     * @brief Getter for the name of the character
+     * @return std::string const&
+     */
+    std::string const & getName(/**There's no input parameter here.*/) const;
+    /**
+     * @brief Getter for character name
+     * @return int const& 
+     */
+    int const & getATK(/**There's no input parameter here.*/) const;
+    /**
+     * @brief Getter for character HP
+     * @return int const& 
+     */
+    int const & getHP(/**There's no input parameter here.*/) const;
+    /**
+     * @brief Getter for attack speed cooldown of character
+     * @return  double const&
+     */
+    double const & getACD(/**There's no input parameter here.*/) const;
+    /**
+     * @brief Function for depleting HP-s during fight
+     * Basic subtraction operation. If the HP drops below 0 we set it to 0
+     * @param[in] Character type object
+     */
     void sufferDamage(Character*);
-    bool isDead() const;
+    /**
+     * @brief Bool function to examine if the Character is dead or alive
+     * @return true, if the Characters' HP 0 or below 0
+     * @return false, if the Characters' HP is more than 0
+     */
+    bool isDead(/**There's no input parameter here.*/) const;
+    /**
+     * @brief Fight function, performs the whole fight
+     * Winner gets announced. Puts out the results to both file and command line
+     * @param[in] Character type object
+     */
     void fight(Character*);
-    //'<<'operator overload
+    /**
+     * @brief Operator overload
+     * Printing a characters' status
+     * @param[in] output stream 
+     * @param[in] character (name, HP, damage)
+     * @return std::ostream& (containing Characters' status)
+     */
     friend std::ostream& operator<<(std::ostream& os, const Character& character);
 
 protected:
@@ -30,4 +81,4 @@ protected:
     const double ACD;
 };
 
-#endif // CHARACTER_HEADER
+#endif
--- Utility.h
+++ Utility.h
@@ -3,10 +3,8 @@
  * @author lapatkrisz
  * @class Utility
  * 
- * @brief Utility class
+ * @brief Utility namespace
  * 
- * 
- *  
  * @version 0.3
  * @date 2020-10-11
  */
@@ -18,9 +16,24 @@
 #include "Character.h"
 
 namespace Utility {
+    /**
+     * @brief Method for checking if a given input is a number or not
+     * @param[in] string
+     * @return true, if we get numbers correctly
+     * @return false, if the input's size is 0 or it isn't number
+     */
     bool isNumber(const std::string&);
+    /**
+     * @brief Method for splitting a string by specific characters
+     * @param[in] string, char
+     * @return Output vector which contains strings
+     */
     std::vector<std::string> split(const std::string&, char);
+    /**
+     * @brief Delete every existing Character object given as input
+     * @param[in] Character vector
+     */
     void deleteCharacters(const std::vector<Character*>&);
 };
 
-#endif // UTILITY_HEADER
+#endif
