c7f85310e91fbb3218c92834b7ba104f7426841f
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-10-15 16:32:15 +0200

    Create test for parseStream function

--- tests/parserTests.cpp
+++ tests/parserTests.cpp
@@ -12,6 +12,11 @@ TEST(parserTests, TypeParseCheck) {
     ASSERT_EQ(std::any_cast<float>(parsedJson["float"]), 15.05f);
 }
 
+TEST(parserTests, ParseFromStream) {
+    std::ifstream f("../units.Tolvaj.json");
+    ASSERT_NO_THROW(Utility::parseStream(f));
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
