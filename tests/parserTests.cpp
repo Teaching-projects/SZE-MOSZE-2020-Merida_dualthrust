75c300e2e435f2dae8be906ae2504ec62285cce8
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-10-21 13:06:15 +0200

    Create JSON tests for badly formatted JSON inputs

--- tests/parserTests.cpp
+++ tests/parserTests.cpp
@@ -6,6 +6,21 @@ TEST(parserTests, parseFile) {
     ASSERT_NO_THROW(Utility::parseFile("../units/Tolvaj.json"));
 }
 
+TEST(parserTests, badFormat_missing_doubledot) {
+    std::map<std::string, std::any> parsedJson = Utility::parseString("{ \"str\" \"Robin\",\"float\" : 15.05 }");
+    ASSERT_EQ(parsedJson, {});
+}
+
+TEST(parserTests, badFormat_missing_curly_bracket) {
+    std::map<std::string, std::any> parsedJson = Utility::parseString(" \"str\" : \"Robin\",\"float\" : 15.05 }");
+    ASSERT_EQ(parsedJson, {});
+}
+
+TEST(parserTests, badFormat_missing_apostrophe) {
+    std::map<std::string, std::any> parsedJson = Utility::parseString(" \"str\" : Robin,\"float\" : 15.05 }");
+    ASSERT_EQ(parsedJson, {});
+}
+
 TEST(parserTests, TypeParseCheck) {
     std::map<std::string, std::any> parsedJson = Utility::parseString("{ \"str\" : \"Robin\",\"float\" : 15.05 }");
     ASSERT_EQ(std::any_cast<std::string>(parsedJson["str"]), "Robin");
