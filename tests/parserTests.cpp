0b59976b55f9570baeb5388cd74fea1f2bd7b48f
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-11-02 10:11:45 +0100

    Update parserTests.cpp

--- tests/parserTests.cpp
+++ tests/parserTests.cpp
@@ -6,23 +6,6 @@ TEST(parserTests, parseFile) {
     ASSERT_NO_THROW(JSON::parseFile("../units/Tolvaj.json"));
 }
 
-
-TEST(parserTests, badFormat_missing_curly_bracket) {
-    int mapSize = JSON::parseString(" \"str\" : \"Robin\",\"float\" : 15.05 }").size();
-    ASSERT_EQ(mapSize, 0);
-}
-
-TEST(parserTests, badFormat_missing_apostrophe) {
-    int mapSize = JSON::parseString(" \"str\" : Robin,\"float\" : 15.05 }").size();
-    ASSERT_EQ(mapSize, 0);
-}
-
-TEST(parserTests, TypeParseCheck) {
-    std::map<std::string, std::any> parsedJson = JSON::parseString("{ \"str\" : \"Robin\",\"float\" : 15.05 }");
-    ASSERT_EQ(std::any_cast<std::string>(parsedJson["str"]), "Robin");
-    ASSERT_EQ(std::any_cast<float>(parsedJson["float"]), 15.05f);
-}
-
 TEST(parserTests, ParseFromStream) {
     std::ifstream f("../units.Tolvaj.json");
     ASSERT_NO_THROW(JSON::parseStream(f));
