99441889f92af1e1b1a658cb7d8285fcce3c3e61
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-11-02 18:09:13 +0100

    Update JSON.h

--- JSON.h
+++ JSON.h
@@ -7,6 +7,7 @@
  * 
  * @version 1.0
  * @date 2020-10-11
+ * 
  */
 #ifndef JSON_HEADER
 #define JSON_HEADER
@@ -23,7 +24,7 @@
 class JSON {
 public:
     /**
-     * @brief Constructor for JSON object
+     * @brief Constructor for JSON object.
      * 
      */
     JSON(const std::map<std::string, std::string>&);
@@ -37,28 +38,62 @@ public:
 
     int count(std::string);
     /**
-     * @brief Method for checking if a given input is a number or not
+     * @brief Method for checking if a given input is a number or not.
      * @param[in] string
      * @return true, if we get numbers correctly
      * @return false, if the input's size is 0 or it isn't number
+     * 
      */
     bool isNumber(const std::string&);
+
     /**
-     * @brief Method for splitting a string by specific characters
-     * @param[in] string, char
-     * @return Output vector which contains strings
+     * @brief Utility method for handling strings, works similarly to other programming languages' split method.
+     * 
+     * @return std::vector<std::string> 
+     * 
      */
     static std::vector<std::string> split(const std::string&, char);
+    /**
+     * @brief Specific splitting method for JSON files, responsible for finding the JSON rows in a given JSON string.
+     * 
+     * @return std::vector<std::string> 
+     * 
+     */
     static std::vector<std::string> splitRowsJSON(const std::string&);
+    /**
+     * @brief Specific whitespace remover method for JSON files, responsible for removing whitespaces from a given JSON string.
+     * 
+     * @return std::string 
+     * 
+     */
     static std::string removeJSONSpaces(std::string);
 
+    /**
+     * @brief JSON parser method. Takes a filepath as input and returns an object of the JSON class.
+     * 
+     * @return JSON 
+     * 
+     */
     static JSON parseString(const std::string&);
+    /**
+     * @brief JSON parser method, takes an ifstream as an input and returns an object of the JSON class.
+     * 
+     * @return JSON 
+     * 
+     */
     static JSON parseStream(std::ifstream&);
+    /**
+     * @brief JSON parser method, takes a string as an input and returns an object of the JSON class.
+     * 
+     * @return JSON 
+     * 
+     */
     static JSON parseFromFile(const std::string&);
 
     /**
-     * @brief Delete every existing Character object given as input
+     * @brief Delete every existing Character object given as input.
      * @param[in] Character vector
+     * 
      */
     void deleteCharacters(const std::vector<Monster*>&);
     std::vector<std::string> getJsonData(const std::string&);
