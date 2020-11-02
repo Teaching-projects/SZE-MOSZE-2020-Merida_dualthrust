175caf3dae0b5ebba39844fe2593df5a0040c738
Tamás Nemes <nemestomi99@gmail.com>
2020-11-02 09:38:48 +0100

    Adds tests with both scenarios

--- run_test.sh
+++ run_test.sh
@@ -2,9 +2,20 @@
 
 IFS=$'\n'
 
-./a.out scenario1.json >> results.txt
+./a.out scenario1.json >> results_scenario_1.txt
 
-diff results.txt results_correct.txt
+diff results_scenario_1.txt results_correct_scenario_1.txt
+
+if [ $? -eq 0 ]
+then
+echo "Test successful."
+else
+echo "Test failed."
+fi
+
+./a.out scenario2.json >> results_scenario_2.txt
+
+diff results_scenario_2.txt results_correct_scenario_2.txt
 
 if [ $? -eq 0 ]
 then
