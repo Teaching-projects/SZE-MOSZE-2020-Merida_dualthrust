a70ab5878fd10aaf20b0a7ba3388a904e2464993
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-11-03 12:28:23 +0100

    Add mixed order check for JSON tests

--- tests/parserTests.cpp
+++ tests/parserTests.cpp
@@ -1,7 +1,6 @@
 #include "../JSON.cpp"
 
 #include <gtest/gtest.h>
-#include <iostream>
 
 TEST(parserTests, parseFile) {
     ASSERT_NO_THROW(JSON::parseFromFile("../units/Tolvaj.json"));
@@ -25,8 +24,15 @@ TEST(parserTests, Format_oneLine) {
     JSON data = JSON::parseFromFile("./Testfiles/oneline.json");
     ASSERT_EQ(data.get<std::string>("string"), "Merida Dualthrust");
     ASSERT_EQ(data.get<int>("int"), 100);
-    std::cout<<data.get<float>("float")<<std::endl;
     ASSERT_EQ(data.get<float>("float"), 0.1f);
+}
+
+TEST(parserTests, Format_mixed) {
+    JSON normal_order = JSON::parseFromFile("./Testfiles/normal.json");
+    JSON mixed_order  = JSON::parseFromFile("./Testfiles/mixed_order.json");
+    ASSERT_EQ(normal_order.get<std::string>("string"), mixed_order.get<std::string>("string"));
+    ASSERT_EQ(normal_order.get<int>("int"), mixed_order.get<int>("int"));
+    ASSERT_EQ(normal_order.get<float>("float"),mixed_order.get<float>("float"));
 }
 
 int main(int argc, char** argv) {
