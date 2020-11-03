bbd148a97baa7158e18fdcf23634ba99fe1598fe
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-11-03 11:59:36 +0100

    Add typeParse checks

--- tests/parserTests.cpp
+++ tests/parserTests.cpp
@@ -11,6 +11,16 @@ TEST(parserTests, ParseFromStream) {
     ASSERT_NO_THROW(JSON::parseStream(f));
 }
 
+//==========================================================================
+
+TEST(parserTests, TypeCastCheck) {
+    JSON data = JSON::parseFromFile("./Testfiles/normal.json");
+    ASSERT_EQ(data.get<std::string>("string"), "Merida Dualthrust");
+    ASSERT_EQ(data.get<int>("int"), 100);
+    ASSERT_EQ(data.get<int>("float"), 0.1f);
+
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
