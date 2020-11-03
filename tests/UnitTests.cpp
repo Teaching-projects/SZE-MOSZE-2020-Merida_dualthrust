10505faf230575f9813f6cc7d83be3b0f5dfced7
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-11-03 17:26:30 +0100

    Separate Unit and Parser tests

---
+++ tests/UnitTests.cpp
@@ -0,0 +1,52 @@
+#include "../Monster.cpp"
+#include "../Hero.cpp"
+#include <gtest/gtest.h>
+
+//Test for the Monster and Hero class
+
+TEST(unitTests, parseMonster){
+    ASSERT_NO_THROW(Monster::parse("../Zombie.json"));
+}
+
+TEST(unitTests, parseHero){
+    ASSERT_NO_THROW(Hero::parse("../Dark_Wanderer.json"));
+}
+
+TEST(unitTests, isAlive){
+    Monster dead = Monster("Dead",0,0,0);
+    ASSERT_EQ(dead.isAlive(), false);
+}
+
+TEST(unitTests, getHealth){
+    Monster healthy = Monster("Healthy",1000,1000,1000);
+    ASSERT_EQ(healthy.getHealthPoints(), 1000);
+}
+
+TEST(unitTests, getDamage){
+    Monster strong = Monster("strong",1000,1000,1000);
+    ASSERT_EQ(strong.getDamage(), 1000);
+}
+
+TEST(unitTests, sufferDamage){
+    Monster attacker = Monster("Attacker",10,10,10);
+    Monster dead = Monster("Dead",10,10,10);
+    dead.sufferDamage(&attacker);
+    ASSERT_EQ(dead.isAlive(), false);
+}
+
+TEST(unitTests, levelUp){
+    Hero hero = Hero("Hero",100,100,100,100,100,100,2);
+    Monster enemy = Monster("Monster",100,1,1);
+    hero.fightTilDeath(enemy);
+
+    ASSERT_EQ(hero.getLevel(),2);
+    ASSERT_EQ(hero.getHealthPoints(),199);
+    ASSERT_EQ(hero.getAttackCoolDown(),200);
+    ASSERT_EQ(hero.getDamage(),200);
+}
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
