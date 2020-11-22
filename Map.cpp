30e9a37a8060c3817cd9ce4e880147f8080f0f35
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-11-22 18:36:38 +0100

    Create constructor and get function for the Map class

--- Map.cpp
+++ Map.cpp
@@ -4,13 +4,31 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 Map::type Map::get(int x, int y) const
 {
-    
+    if(x >= map.size() || x < 0 ){
+        throw WrongIndexException();
+    }else{
+        if(y >= map[x].length() || y < 0){
+            throw WrongIndexException();
+        }else{
+            std::string types = " #";
+            return type(types.find(map[x][y]));
+        }
+    }
+
 }
 
-Map::Map(std::string filename)
+Map::Map(std::string path)
 {
+    
+    std::string line;
+    std::ifstream mapfile(path);
+    while (std::getline(mapfile, line))
+    {
+        map.push_back(line);
+    }
 
 } // Betolt fajlbol egy terkepet
