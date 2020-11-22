b6b25375e2eedc95de4de33b5a5a336746971d24
Tamás Nemes <nemestomi99@gmail.com>
2020-11-22 11:13:54 +0100

    Adds description for methods

--- Map.h
+++ Map.h
@@ -1,24 +1,67 @@
+/**
+ * @file Map.h
+ * @author NemesTom
+ * @brief Map class
+ * 
+ * This class contains basic methods for handling the map.
+ * 
+ * 
+ * @version 1.0
+ * @date 2020-11-22
+ * 
+ */
+
 #ifndef MAP_HEADER
 #define MAP_HEADER
 
 #include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 class Map
 {
     private:
-
+        /**
+         * @brief Data structure that contains the map.
+         * 
+         */
+        std::vector<std::string> map; 
     public:
-
+        /**
+         * @brief Types that the map may contain.
+         * 
+         */
         enum type 
         {
             Free,
             Wall
         };
-
+        /**
+         * @brief Gives back a map constructed from a txt file.
+         * 
+         * When the given input is incorrect, it returns with an exception.
+         * 
+         * @param filename Name of the input file.
+         * 
+         * @return Map 
+         * 
+         */
+        Map(std::string filename);
+        /**
+         * @brief Gives back the type of terrain based on coordinates.
+         * 
+         * Takes x and y coordinates.
+         * 
+         * @param x Longitudinal coordinate. (Which "column")
+         * @param y Latitudinal coordinate. (Which "row")
+         * @return type 
+         * 
+         */
         Map::type get(int x, int y) const;
 
-        Map(std::string filename); // Betolt fajlbol egy terkepet
-
+        //TODO WrongIndexException 
 };
 
 #endif // MAP_HEADER
