ac477fb31bfc5dec3d23ec4fb4e8cf4876cf88c3
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-11-27 17:11:40 +0000

    Update gh-pages to output generated at 6dfd273

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['damage_2',['Damage',['../structDamage.html',1,'Damage'],['../classMonster.html#a3819d1bf6ee6bf2a9be94f8ddd2d4cb5',1,'Monster::damage()']]],
-  ['damagebonusperlevel_3',['damageBonusPerLevel',['../classHero.html#a6ff46be0ecde98f50f324058a1a289e4',1,'Hero']]],
+  ['damagebonusperlevel_3',['damageBonusPerLevel',['../classHero.html#ace09f226b09843be731f7856af366b38',1,'Hero']]],
   ['defense_4',['defense',['../classMonster.html#ad867513550d7fde7e4b79806fe90ae2f',1,'Monster']]],
   ['defensebonusperlevel_5',['defenseBonusPerLevel',['../classHero.html#a30e1bc3ac558cd7c23f62162092637bf',1,'Hero']]],
   ['deletecharacters_6',['deleteCharacters',['../classJSON.html#a820f6bd7c781c3710592ddc7c66233a3',1,'JSON']]],
