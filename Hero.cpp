8f9e59413e3f03d2c1b431957ec675c91fc7d2e8
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-11-27 18:19:15 +0100

    Update Hero.cpp

--- Hero.cpp
+++ Hero.cpp
@@ -18,18 +18,7 @@
     int const & Hero::getLevel() const
     {
         return level;
-    }
-
-    int const & Hero::getPhysicalDamage() const
-    {
-        return damage.physical;
-    }
-
-    int const & Hero::getMagicalDamage() const
-    {
-        return damage.magical;
-    }
-    
+    }    
 
     void Hero::levelUp()
     {
