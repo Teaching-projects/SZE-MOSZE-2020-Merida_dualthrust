ee92e4e61ace07566665e6e6eef8c204340fb792
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-12-03 19:43:51 +0100

    Update UnitTests.cpp

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -15,36 +15,46 @@ TEST(unitTests, parseHero){
 }
 
 TEST(unitTests, isAlive){
-    Monster dead = Monster("Dead",0,Damage(0,0),0,0);
+    Monster dead = Monster("Dead",0,0,0,0,0);
     ASSERT_EQ(dead.isAlive(), false);
 }
 
 TEST(unitTests, getHealth){
-    Monster healthy = Monster("Healthy",1000,Damage(1000,1000),1000,1000);
+    Monster healthy = Monster("Healthy",1000,1000,1000,1000,1000);
     ASSERT_EQ(healthy.getHealthPoints(), 1000);
 }
 
 TEST(unitTests, getPhysicalDamage){
-    Monster strong = Monster("strong",1000,Damage(1000,1000),1000,1000);
+    Monster strong = Monster("strong",1000,1000,1000,1000,1000);
     ASSERT_EQ(strong.getPhysicalDamage(), 1000);
+}
+TEST(unitTests, getMagicalDamage){
+    Monster stronger = Monster("stronger",1000,1000,1000,1000,1000);
+    ASSERT_EQ(stronger.getMagicalDamage(), 1000);
+}
+TEST(unitTests, getDefense){
+    Monster defender = Monster("defender",1000,1000,1000,1000,1000);
+    ASSERT_EQ(defender.getDefense(), 1000);
 }
 
 TEST(unitTests, sufferDamage){
-    Monster attacker = Monster("Attacker",10,Damage(10,10),10,10);
-    Monster dead = Monster("Dead",1,Damage(1,1),1,1);
+    Monster attacker = Monster("Attacker",10,10,10,10,10);
+    Monster dead = Monster("Dead",1,1,1,1,1);
     dead.sufferDamage(&attacker);
     ASSERT_EQ(dead.isAlive(), false);
 }
 
 TEST(unitTests, levelUp){
-    Hero hero = Hero("Hero",100,Damage(100,100),100,100,100,100,100,100,2);
-    Monster enemy = Monster("Monster",100,Damage(1,1),1,1);
+    Hero hero = Hero("Hero",100,100,100,100,100,100,100,100,100,2);
+    Monster enemy = Monster("Monster",100,1,1,1,1);
     hero.fightTilDeath(enemy);
 
     ASSERT_EQ(hero.getLevel(),2);
     ASSERT_EQ(hero.getHealthPoints(),200);
     ASSERT_EQ(hero.getAttackCoolDown(),200);
     ASSERT_EQ(hero.getPhysicalDamage(),200);
+    ASSERT_EQ(hero.getMagicalDamage(),200);
+    ASSERT_EQ(hero.getDefense(),200);
 }
 
 
