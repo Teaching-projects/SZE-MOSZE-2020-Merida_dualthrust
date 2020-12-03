b5fde2ba9c23c0e38f0b2cecc4104e4982aa1929
NemesTom <nemestomi99@gmail.com>
2020-12-03 10:14:09 +0000

    Update gh-pages to output generated at 82b9512

--- search/all_a.js
+++ search/all_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['map_39',['Map',['../classMap.html',1,'Map'],['../classMap.html#a0f5ad0fd4563497b4214038cbca8b582',1,'Map::Map()'],['../classMap.html#ab124c12c6c9dec693aedb1a4b6384ecd',1,'Map::Map(std::string filename)']]],
-  ['map_2eh_40',['Map.h',['../Map_8h.html',1,'']]],
-  ['maximumhealthpoint_41',['maximumHealthPoint',['../classHero.html#a0332b15316cbf518ebac8eba594d1caa',1,'Hero']]],
-  ['monster_42',['Monster',['../classMonster.html',1,'Monster'],['../classMonster.html#a89df1fa81c57037fce4f85058dee46c5',1,'Monster::Monster()']]],
-  ['monster_2eh_43',['Monster.h',['../Monster_8h.html',1,'']]]
+  ['map_36',['Map',['../classMap.html',1,'Map'],['../classMap.html#a0f5ad0fd4563497b4214038cbca8b582',1,'Map::Map()'],['../classMap.html#ab124c12c6c9dec693aedb1a4b6384ecd',1,'Map::Map(std::string filename)']]],
+  ['map_2eh_37',['Map.h',['../Map_8h.html',1,'']]],
+  ['markedmap_38',['MarkedMap',['../classMarkedMap.html',1,'']]],
+  ['maximumhealthpoint_39',['maximumHealthPoint',['../classHero.html#a0332b15316cbf518ebac8eba594d1caa',1,'Hero']]],
+  ['monster_40',['Monster',['../classMonster.html',1,'Monster'],['../classMonster.html#a89df1fa81c57037fce4f85058dee46c5',1,'Monster::Monster()']]],
+  ['monster_2eh_41',['Monster.h',['../Monster_8h.html',1,'']]]
 ];
