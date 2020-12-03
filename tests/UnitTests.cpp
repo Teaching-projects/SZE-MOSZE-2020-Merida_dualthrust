e12e2906eea4327908b41799c25e050cf26d66f6
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-12-03 22:08:49 +0100

    UnitTest tryout

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -15,38 +15,47 @@ TEST(unitTests, parseHero){
 }
 
 TEST(unitTests, isAlive){
-    Monster dead = Monster("Dead",0,0,0,0,0);
+    Damage tmp (0, 0);
+    Monster dead = Monster("Dead",0,tmp,0,0);
     ASSERT_EQ(dead.isAlive(), false);
 }
 
 TEST(unitTests, getHealth){
-    Monster healthy = Monster("Healthy",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster healthy = Monster("Healthy",1000,tmp,1000,1000);
     ASSERT_EQ(healthy.getHealthPoints(), 1000);
 }
 
 TEST(unitTests, getPhysicalDamage){
-    Monster strong = Monster("strong",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster strong = Monster("strong",1000,tmp,1000,1000);
     ASSERT_EQ(strong.getPhysicalDamage(), 1000);
 }
 TEST(unitTests, getMagicalDamage){
-    Monster stronger = Monster("stronger",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster stronger = Monster("stronger",1000,tmp,1000,1000);
     ASSERT_EQ(stronger.getMagicalDamage(), 1000);
 }
 TEST(unitTests, getDefense){
-    Monster defender = Monster("defender",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster defender = Monster("defender",1000,tmp,1000,1000);
     ASSERT_EQ(defender.getDefense(), 1000);
 }
 
 TEST(unitTests, sufferDamage){
-    Monster attacker = Monster("Attacker",10,10,10,10,10);
-    Monster dead = Monster("Dead",1,1,1,1,1);
+    Damage tmp_1 (10, 10);
+    Monster attacker = Monster("Attacker",10,tmp_1,10,10);
+    Damage tmp_2 (1, 1);
+    Monster dead = Monster("Dead",1,tmp_2,1,1);
     dead.sufferDamage(&attacker);
     ASSERT_EQ(dead.isAlive(), false);
 }
 
-/*TEST(unitTests, levelUp){
-    Hero hero = Hero("Hero",100,100,100,100,100,100,100,100,100,2);
-    Monster enemy = Monster("Monster",100,1,1,1,1);
+TEST(unitTests, levelUp){
+    Damage tmp_1 (100, 100);
+    Hero hero = Hero("Hero",100,tmp_1,100,100,100,100,100,100,2);
+    Damage tmp_2 (1, 1);
+    Monster enemy = Monster("Monster",100,tmp_2,1,1);
     hero.fightTilDeath(enemy);
 
     ASSERT_EQ(hero.getLevel(),2);
@@ -55,7 +64,7 @@ TEST(unitTests, sufferDamage){
     ASSERT_EQ(hero.getPhysicalDamage(),200);
     ASSERT_EQ(hero.getMagicalDamage(),200);
     ASSERT_EQ(hero.getDefense(),200);
-}*/
+}
 
 
 int main(int argc, char** argv) {
