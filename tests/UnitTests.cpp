61c19f0f6534b8b31c183c791fda7b6b38317622
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-12-03 20:40:18 +0100

    Update UnitTests.cpp

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -15,31 +15,37 @@ TEST(unitTests, parseHero){
 }
 
 TEST(unitTests, isAlive){
-    Monster dead = Monster("Dead",0,0,0,0,0);
+    Damage tmp (0, 0);
+    Monster dead = Monster("Dead",0,tmp,0,0,);
     ASSERT_EQ(dead.isAlive(), false);
 }
 
 TEST(unitTests, getHealth){
-    Monster healthy = Monster("Healthy",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster healthy = Monster("Healthy",1000,tmp,1000,1000);
     ASSERT_EQ(healthy.getHealthPoints(), 1000);
 }
 
 TEST(unitTests, getPhysicalDamage){
-    Monster strong = Monster("strong",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster strong = Monster("strong",1000,tmp,1000,1000);
     ASSERT_EQ(strong.getPhysicalDamage(), 1000);
 }
 TEST(unitTests, getMagicalDamage){
-    Monster stronger = Monster("stronger",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster stronger = Monster("stronger",1000,tmp,1000,1000);
     ASSERT_EQ(stronger.getMagicalDamage(), 1000);
 }
 TEST(unitTests, getDefense){
-    Monster defender = Monster("defender",1000,1000,1000,1000,1000);
+    Damage tmp (1000, 1000);
+    Monster defender = Monster("defender",1000,tmp,1000,1000);
     ASSERT_EQ(defender.getDefense(), 1000);
 }
 
 TEST(unitTests, sufferDamage){
-    Monster attacker = Monster("Attacker",10,10,10,10,10);
-    Monster dead = Monster("Dead",1,1,1,1,1);
+    Damage tmp (10, 10);
+    Monster attacker = Monster("Attacker",10,tmp,10,10);
+    Monster dead = Monster("Dead",1,tmp,1,1);
     dead.sufferDamage(&attacker);
     ASSERT_EQ(dead.isAlive(), false);
 }
@@ -47,7 +53,7 @@ TEST(unitTests, sufferDamage){
 TEST(unitTests, levelUp){
     Damage tmp (10, 10);
     Hero hero = Hero("Hero",100,tmp,100,100,100,100,100,100,100,2);
-    Monster enemy = Monster("Monster",1,tmp,1,1,1);
+    Monster enemy = Monster("Monster",1,tmp,1,1);
     hero.fightTilDeath(enemy);
 
     ASSERT_EQ(hero.getLevel(),2);
