ffff0cad4391cb8f0a6d2ebc13c00be2e3adce08
NemesTom <nemestomi99@gmail.com>
2020-12-04 15:00:49 +0000

    Update gh-pages to output generated at 1df71ec

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['json_32',['JSON',['../classJSON.html',1,'JSON'],['../classJSON.html#aba4f484f3ee84128f50ffd4d3b20ccf1',1,'JSON::JSON()']]],
-  ['json_2eh_33',['JSON.h',['../JSON_8h.html',1,'']]]
+  ['json_33',['JSON',['../classJSON.html',1,'JSON'],['../classJSON.html#aba4f484f3ee84128f50ffd4d3b20ccf1',1,'JSON::JSON()']]],
+  ['json_2eh_34',['JSON.h',['../JSON_8h.html',1,'']]]
 ];
--- search/variables_5.js
+++ search/variables_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['map_139',['map',['../classMap.html#a7fbb34f989fcf03bb5717073f882080a',1,'Map']]],
-  ['maximumhealthpoint_140',['maximumHealthPoint',['../classHero.html#a0332b15316cbf518ebac8eba594d1caa',1,'Hero']]]
+  ['maximumhealthpoint_143',['maximumHealthPoint',['../classHero.html#a0332b15316cbf518ebac8eba594d1caa',1,'Hero']]]
 ];
