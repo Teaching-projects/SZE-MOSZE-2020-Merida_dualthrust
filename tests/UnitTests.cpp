b09ff5e6671a513ea341bdb754b5c187e386b6ff
LapatKrisz <57176174+LapatKrisz@users.noreply.github.com>
2020-12-06 19:26:42 +0100

    Update UnitTests.cpp

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -64,21 +64,17 @@ TEST(unitTests, sufferDamage){
 
 TEST(unitTests, levelUp){
     Damage dmg;
-    dmg.physical=100;
-    dmg.magical=100;
-    Hero hero = Hero("Hero",100,dmg,100,100,100,100,100,100,100,2);
-    Damage dmg_2;
-    dmg_2.physical=1;
-    dmg_2.magical=1;
-    Monster enemy = Monster("Monster",100,dmg_2,1,1);
-    hero.fightTilDeath(enemy);
+    dmg.physical=1;
+    dmg.magical=1;
+    Hero hero = Hero("Hero",1,dmg,1,1,1,1,1,1,1,2);
+    hero.levelUp()
 
     ASSERT_EQ(hero.getLevel(),2);
-    ASSERT_EQ(hero.getHealthPoints(),200);
-    ASSERT_EQ(hero.getAttackCoolDown(),200);
-    ASSERT_EQ(hero.getPhysicalDamage(),100);
-    ASSERT_EQ(hero.getMagicalDamage(),100);
-    ASSERT_EQ(hero.getDefense(),200);
+    ASSERT_EQ(hero.getHealthPoints(),2);
+    ASSERT_EQ(hero.getAttackCoolDown(),2);
+    ASSERT_EQ(hero.getPhysicalDamage(),2);
+    ASSERT_EQ(hero.getMagicalDamage(),2);
+    ASSERT_EQ(hero.getDefense(),2);
 }
 
 
