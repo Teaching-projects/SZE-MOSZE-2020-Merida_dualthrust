628bde31693420edaf96b03465e8ade354ca88b4
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2020-12-07 11:46:50 +0100

    Fix unittest

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -17,52 +17,31 @@ TEST(unitTests, parseHero){
 }
 
 TEST(unitTests, isAlive){
-    Damage dmg;
-    dmg.physical=100;
-    dmg.magical=100;
-    Monster dead = Monster("Dead",0,dmg,0,0);
+    Monster dead = Monster("Dead",0,Damage(100,100),0,0);
     ASSERT_EQ(dead.isAlive(), false);
 }
 
 TEST(unitTests, getHealth){
-    Damage dmg;
-    dmg.physical=1000;
-    dmg.magical=1000;
-    Monster healthy = Monster("Healthy",1000,dmg,1000,1000);
+    Monster healthy = Monster("Healthy",1000,Damage(1000,1000),1000,1000);
     ASSERT_EQ(healthy.getHealthPoints(), 1000);
 }
 
 TEST(unitTests, getPhysicalDamage){
-    Damage dmg;
-    dmg.physical=1000;
-    dmg.magical=1000;
-    Monster strong = Monster("strong",1000,dmg,1000,1000);
+    Monster strong = Monster("strong",1000,Damage(1000,1000),1000,1000);
     ASSERT_EQ(strong.getPhysicalDamage(), 1000);
 }
 TEST(unitTests, getMagicalDamage){
-    Damage dmg;
-    dmg.physical=1000;
-    dmg.magical=1000;
-    Monster stronger = Monster("stronger",1000,dmg,1000,1000);
+    Monster stronger = Monster("stronger",1000,Damage(1000,1000),1000,1000);
     ASSERT_EQ(stronger.getMagicalDamage(), 1000);
 }
 TEST(unitTests, getDefense){
-    Damage dmg;
-    dmg.physical=1000;
-    dmg.magical=1000;
-    Monster defender = Monster("defender",1000,dmg,1000,1000);
+    Monster defender = Monster("defender",1000,Damage(1000,1000),1000,1000);
     ASSERT_EQ(defender.getDefense(), 1000);
 }
 
 TEST(unitTests, sufferDamage){
-    Damage dmg;
-    dmg.physical=10;
-    dmg.magical=10;
-    Monster attacker = Monster("Attacker",10,dmg,10,10);
-    Damage dmg_2;
-    dmg_2.physical=100;
-    dmg_2.magical=100;
-    Monster dead = Monster("Dead",1,dmg_2,1,1);
+    Monster attacker = Monster("Attacker",10,Damage(10,10),10,10);
+    Monster dead = Monster("Dead",1,Damage(100,100),1,1);
     dead.sufferDamage(&attacker);
     ASSERT_EQ(dead.isAlive(), false);
 }
