00925e3c510534f7bf36b4b5e2d50d0e44e84f07
Tamás Nemes <nemestomi99@gmail.com>
2020-12-09 21:16:53 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ IFS=$'\n'
 ./a.out preparedgame.json -test
 
 
-diff results_correct_preparedgame.txt ./renderer outputs/hero_text_output.txt
+diff results_correct_preparedgame.txt renderer outputs/hero_text_output.txt
 
 if [ $? -eq 0 ]
 then
